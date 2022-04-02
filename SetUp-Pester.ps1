b951b210a128d159f54646572a9e6ccb033403fa
Kevincash99 <43646364+Kevincash99@users.noreply.github.com>
2022-04-02 16:52:05 -0400

    Add files via upload

---
+++ SetUp-Pester.ps1
@@ -0,0 +1,38 @@
+﻿#Get Version 
+$PSVersionTable
+
+# Import tools
+Import-Module PackageManagement 
+
+#dispaly commands in PackageManagement 
+get-command -Module PackageManagement 
+
+#Shows The  Package in Repostory 
+Find-Package Pester
+
+#Install Pester
+
+Install-Module Pester -Force
+
+#Update-Module Pester
+Update-Module Pester 
+
+
+#Pester Test 
+Describe 'Test Name'{
+     It 'Has this condition'{
+     'test for condition' | Should xxx
+     }
+
+
+}
+
+#region
+
+Should
+
+Filenamae.xyz | Should Exist
+Filenamae.xyz | Should Contain  'Some Text'
+Condition ($X -gt 1) | Should be $True 
+
+#endregion
