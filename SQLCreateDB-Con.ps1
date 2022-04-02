e5b2ddadab09d7f1433675eb22dc288bc7b1e1ca
Kevincash99 <43646364+Kevincash99@users.noreply.github.com>
2022-04-02 16:52:27 -0400

    Add files via upload

---
+++ SQLCreateDB-Con.ps1
@@ -0,0 +1,38 @@
+﻿get-nettcpconnection | where {($_.State -eq "Listen") -and ($_.RemoteAddress -eq "0.0.0.0")}
+[String]$dbname = "MyNewDatabase";
+
+
+ 
+# Open ADO.NET Connection with Windows authentification to local SQLSERVER.
+$con = New-Object Data.SqlClient.SqlConnection;
+$con.ConnectionString = "Data Source=.;Initial Catalog=master;Integrated Security=True;";
+$con.Open();
+ 
+# Select-Statement for AD group logins
+$sql = "SELECT name
+        FROM sys.databases
+        WHERE name = '$dbname';";
+ 
+# New command and reader.
+$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+$rd = $cmd.ExecuteReader();
+if ($rd.Read())
+{   
+    Write-Host "Database $dbname already exists";
+    Return;
+}
+ 
+$rd.Close();
+$rd.Dispose();
+ 
+# Create the database.
+$sql = "CREATE DATABASE [$dbname];"
+$cmd = New-Object Data.SqlClient.SqlCommand $sql, $con;
+$cmd.ExecuteNonQuery();     
+Write-Host "Database $dbname is created!";
+ 
+ 
+# Close & Clear all objects.
+$cmd.Dispose();
+$con.Close();
+$con.Dispose();
